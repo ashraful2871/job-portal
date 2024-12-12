eee6cd239589a4a1dd1c751c678303d10c8c2302
ashraful2871 <ashrafulislam2758@gmail.com>
2024-12-13 01:13:38 +0600

    add functionality update status and add sweet aleert

--- src/pages/viewApplication/ViewApplications.jsx
+++ src/pages/viewApplication/ViewApplications.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ViewApplications = () => {
+  const handleStatusUpdate = (e, id) => {
+    console.log(e.target.value, id);
+    const data = {
+      status: e.target.value,
+    };
+    fetch(`http://localhost:5000/job-applications/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          Swal.fire({
+            // position: "top-end",
+            icon: "success",
+            title: "status has been updated",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   const applications = useLoaderData();
   return (
     <div>
@@ -16,7 +43,7 @@ const ViewApplications = () => {
               <th></th>
               <th>Email</th>
               <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Update status</th>
             </tr>
           </thead>
           <tbody>
@@ -25,7 +52,19 @@ const ViewApplications = () => {
                 <th>{idx + 1}</th>
                 <td>{app.applicant_email}</td>
                 <td>Quality Control Specialist</td>
-                <td>Blue</td>
+                <td>
+                  <select
+                    onChange={(e) => handleStatusUpdate(e, app._id)}
+                    defaultValue={app.status || "Change Status"}
+                    className="select select-bordered select-xs w-full max-w-xs"
+                  >
+                    <option disabled>Change Status</option>
+                    <option>Under Review</option>
+                    <option>Set Interview</option>
+                    <option>Hired</option>
+                    <option>Rejected</option>
+                  </select>
+                </td>
               </tr>
             ))}
           </tbody>
